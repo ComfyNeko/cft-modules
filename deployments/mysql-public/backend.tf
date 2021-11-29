3965513cfe66dcc9b04fc99c7b07afa4fdb5e1c3
keerthanasrikumar <keerthana.s1298@gmail.com>
2021-11-29 04:23:44 -0600

    removed some files

--- deployments/mysql-public/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "state-rosh"
-    prefix  = "terraform/mysql-public"
-  }
-}
--- deployments/subnets/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "state-rosh"
-    prefix  = "terraform/subnets"
-  }
-}
